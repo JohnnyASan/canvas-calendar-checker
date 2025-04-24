129340c74bbf244747b954322989534993f0f274
JohnnyASan <johnanthonysanabria@gmail.com>
2025-04-24 14:42:26 -0600

    Persists state across browser refreshes

--- scripts.js
+++ scripts.js
@@ -15,22 +15,28 @@ function addCheckBoxes() {
         const checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
         checkbox.style.marginRight = '10px';
-        checkbox.classList.add('custom-checked');
-        console.log("Checkbox created!");
+        checkbox.style.width = '20px';
+        checkbox.style.height = '20px';
     
         // Load saved state from local storage
         const itemId = item.getAttribute('data-event-id');
         checkbox.checked = localStorage.getItem(itemId) === 'true';
 
+        const container = item.querySelector('.agenda-event__item-container');
+        const assignmentTitle = item.querySelector('.agenda-event__title');
+        if (assignmentTitle.classList.contains(strikethrough_class)) {
+            checkbox.checked = true;
+        } else {
+            checkbox.addEventListener('click', (event) => {
+                event.stopPropagation();
+                localStorage.setItem(itemId, checkbox.checked);
+                assignmentTitle.classList.toggle(strikethrough_class, checkbox.checked);
+                
+            });
+        }
+    
         
-    
-        checkbox.addEventListener('click', (event) => {
-            event.stopPropagation();
-            localStorage.setItem(itemId, checkbox.checked);
-            item.style.textDecoration = checkbox.checked ? 'line-through' : 'none';
-        });
-    
-        item.appendChild(checkbox);
+        container.append(checkbox)
     });
 
 } 
